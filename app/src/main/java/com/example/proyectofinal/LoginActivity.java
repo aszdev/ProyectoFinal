0da4b815f1c8b6b289d652b5a5d0aaa823337d63
ASz <asanchezm10@miumg.edu.gt>
2021-10-10 11:09:25 -0600

    Se agrego la funcion login, y la pantalla de incio

--- app/src/main/java/com/example/proyectofinal/LoginActivity.java
+++ app/src/main/java/com/example/proyectofinal/LoginActivity.java
@@ -2,13 +2,76 @@ package com.example.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        TextInputLayout vemail = (TextInputLayout)findViewById(R.id.usuarioTextField);
+        TextInputLayout vpass = (TextInputLayout)findViewById(R.id.contrasenaTextField);
+        String txt_emal = vemail.getEditText().getText().toString();
+        String txt_pass = vpass.getEditText().getText().toString();
+
+        Button vbtnLog = (Button)findViewById(R.id.inicioSesion);
+        vbtnLog.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                boolean flagh = validarUsuario(txt_emal,txt_pass);
+            }
+        });
     }
+
+    private boolean validarUsuario(String pmail, String ppass){
+
+        if(TextUtils.isEmpty(pmail)){
+            mostrarAlerta("Error! debe ingresar el Email");
+            return  false;
+        }
+        if(TextUtils.isEmpty(ppass)){
+            mostrarAlerta("Error! debe ingresar la Contraseña");
+            return  false;
+        }
+
+////--------------aqui va tu funcion de llamada al web service
+        int idusuario = existeUsuariows(pmail,ppass);
+
+        if(idusuario>0){
+            mostrarAlerta("Bienvenido!!!");
+            Intent intent = new Intent(LoginActivity.this, DashBoard.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }else{
+            mostrarAlerta("Error Usuario o Contraseña incorrectos");
+            return false;
+        }
+
+    }
+
+    /// esta funcion debe regrar el valor del web service y el valor se lo asignas
+    /// a la variable vretorna
+    private int existeUsuariows(String pmail, String ppass){
+        int vretorna =1;
+
+
+        return vretorna;
+    }
+
+    private void mostrarAlerta(String mensaje){
+        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
